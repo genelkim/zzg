e804f05d7605382e12d8a9094b2f760ed9efbb79
Gene Kim <gkim21@cs.rochester.edu>
2017-04-29 18:36:26 -0400

    Added q-states

--- tf_project.py
+++ tf_project.py
@@ -1,23 +1,21 @@
-
+from __future__ import division
 import numpy as np
-np.random.seed(1335) # for reproducibility
+import numpy.random as random
+#random.seed(1335) # for reproducibility
 #np.set_printoptions(precision=5, suppress=True, linewidth=150)
 
 from matplotlib import pyplot as plt
 import tensorflow as tf
-import numpy.random as random
+from collections import namedtuple
 
+AgentState = namedtuple("AgentState", ["cash", "stocks"])
+INITIAL_AGENT_STATE = AgentState(60000, 100) # Cash/stocks
+RewardHistory = namedtuple("RewardHistory", ["single", "window", "final"])
+#
+# Load Data
+#
 
-
-
-
-
-
-
-
-
-
-
+datafile = 'data/XBTEUR_1day.csv'
 
 # Data format:
 # Open, Close, Low, High, Volume
@@ -25,75 +23,199 @@ import numpy.random as random
 # Data subset
 #data = data[:10000]
 #NUM_FEATURES = 5
-data = np.genfromtxt('data/XBTEUR_1day.csv', delimiter=",")
-NUM_FEATURES = 7
+raw_data = np.genfromtxt('data/XBTEUR_1day.csv', delimiter=",")
+# Rescale data.
+means = np.mean(raw_data, axis=0)
+variances = np.var(raw_data, axis=0)
+normed_data = [x - means for x in raw_data]
+normed_data = np.array([x / variances for x in normed_data])
+# Placeholder for qstate values.
+qstate_zeros = np.zeros((raw_data.shape[0], len(INITIAL_AGENT_STATE))) 
+
+data = np.append(raw_data, normed_data, axis=1)
+data = np.append(data, qstate_zeros, axis=1)
 
 
 #
 # Global Parameters.
 #
 
+# Actions.
 BUY_ACT = 2
 SELL_ACT = 0
 HOLD_ACT = 1
 ACTION_LIST = [SELL_ACT, HOLD_ACT, BUY_ACT]
 NUM_ACTIONS = len(ACTION_LIST)
-ACTION_VAL = { BUY_ACT : 100.0, SELL_ACT : -100.0, HOLD_ACT : 0.0 }
+ACTION_VAL = { BUY_ACT : 1, SELL_ACT : -1, HOLD_ACT : 0.0 }
 
+# Q-Learning.
+EPOCHS = 1500
+BUFFER_SIZE = 200
+# Gene: I set discout factor to 0 since our current action don't actually cause a state transition.
+gamma = 1.0/EPOCHS  # discount factor
+#gamma = 0
+alpha = 0.1     # learning rate
+epsilon = 1   # exploration factor
+WINDOW_SIZE = 15
+
+FINAL_REWARD_FACTOR = 1.0
+WINDOW_REWARD_FACTOR = 1.0 / ((len(data) / WINDOW_SIZE) * 10)
+SINGLE_REWARD_FACTOR = 1.0 / (len(data) * 100)
+
+
+# LSTM.
+NUM_FEATURES = data.shape[1]
+#LSTM_HIDDEN_SIZE = 64
+LSTM_HIDDEN_SIZE = NUM_FEATURES
+LSTM_STATE_SIZE = NUM_FEATURES
+FULL_LSTM_STATE_SIZE = 2*LSTM_STATE_SIZE
+BATCH_SIZE = 1
+STEPSIZE = 1 
+
+print "Parameters"
+print "gamma", gamma
+print "alpha", alpha
+print "epsilon", epsilon
+print "buffer size", BUFFER_SIZE
+print "EPOCHS", EPOCHS
+print "file", datafile
+
+print ""
+print "SHORT UPDATE"
+#print "LONG UPDATE"
+
+#
+# Helper Functions
+#
 
 # Mean Squared Error Loss Function for TensorFlow.
 def mse_loss_fn(prediction, gold):
   return tf.sqrt(tf.reduce_sum(tf.pow(tf.subtract(tf.transpose(prediction), gold), 2)))
+
+# Python MSE
+def mse_loss_py(prediction, gold):
+  return np.sqrt(np.sum(np.power(prediction.T - gold, 2)))
 
 # Index of data for price(close)
 PRICE_INDEX = 1
 
 # Reward is difference in price(close) difference * \
 #           action (number of shares sold/bought)
-def get_reward(action, t, data):
+def simple_reward(action, t, data):
   return (data[t][PRICE_INDEX] - data[t-1][PRICE_INDEX])*ACTION_VAL[action]
 
+# Reward based on agent state of cash + stock count.
+# Stock >= 0.
+# 3 different types of rewards:
+#   1. Final reward (at end of epoch, gets a large reward/cost based off of state)
+#   2. Window reward (at the end of each window, a medium reward based off of state)
+#   3. Single reward (each step gets a small reward)
+def stateful_reward(action, t, data, agent_state, past_reward):
+  # Value of current state.
+  state_val = agent_state.cash + agent_state.stocks*data[t][PRICE_INDEX]
+  if agent_state.stocks < 0:
+    state_val = -100000000
+
+  total_reward = 0
+  new_reward = list(past_reward)
+  # Compute final reward.
+  if t == len(data) - 1:
+    total_reward += (state_val - past_reward.final)*FINAL_REWARD_FACTOR
+    new_reward[2] = state_val
+  if t % WINDOW_SIZE == 0:
+    total_reward += (state_val - past_reward.window)*WINDOW_REWARD_FACTOR
+    new_reward[1] = state_val
+
+  total_reward += (state_val - past_reward.single)*SINGLE_REWARD_FACTOR
+  new_reward[0] = state_val
+
+  return total_reward, RewardHistory(new_reward[0], new_reward[1], new_reward[2])
+  
+
+# Reward function.
+def get_reward(action, t, data, agent_state, past_reward):
+  return stateful_reward(action, t, data, agent_state, past_reward)
+  
 
 # Evaluate performance on optimal strategy (no epsilon or random action).
 def max_reward(sess, data):
-  lstate = np.random.normal(0, 0.2, 2*LSTM_HIDDEN_SIZE).astype(np.float32).reshape((1,2*NUM_FEATURES))
+  agent_state = INITIAL_AGENT_STATE
+
+  initial_value = agent_state.cash + (agent_state.stocks * data[0][PRICE_INDEX])
+  past_reward = RewardHistory(initial_value, initial_value, initial_value)
+  
+  lstate = np.random.normal(0, 0.2, FULL_LSTM_STATE_SIZE).astype(np.float32).reshape((1,FULL_LSTM_STATE_SIZE))
   state = data[0]
+  state[-2] = agent_state.cash
+  state[-1] = agent_state.stocks
   reward_sum = 0
+  total_loss = 0
 
   rewards = []
   actions = []
-  for t in xrange(1, len(data)):
+
+  # Initial step.
+  qvals_eval, new_lstm_state_eval = \
+      sess.run([qvals, new_lstm_state], feed_dict={inputs:state.reshape((1,NUM_FEATURES)), lstm_state:lstate})
+  action = np.argmax(qvals_eval)
+  actions.append(action)
+  
+  # Store oldvals,
+  old_qvals = qvals_eval
+  old_state = state
+  
+  #rewards.append(reward)
+  lstate = new_lstm_state_eval
+  state = data[1]
+  state[-2] = old_state[-2] + ACTION_VAL[action]
+  state[-1] = old_state[-1] + ACTION_VAL[action]*old_state[PRICE_INDEX]
+  agent_state = AgentState(new_state[-2], new_state[-1])
+  
+  
+  reward, past_reward = get_reward(action, 1, data, agent_state, past_reward)
+  reward_sum += reward
+
+
+  # Compute prediction and gold in tandem.
+  for t in xrange(2, len(data)):
+    # Gold computation.
     qvals_eval, new_lstm_state_eval = \
         sess.run([qvals, new_lstm_state], feed_dict={inputs:state.reshape((1,NUM_FEATURES)), lstm_state:lstate})
+    # Compute loss.
+    max_qval = np.max(qvals_eval)
+    #update = old_qvals[action] + alpha * (reward + gamma * max_qval - old_qvals[action])
+    update = alpha * (reward + (gamma * max_qval))
+    y = np.zeros((NUM_ACTIONS, 1))
+    y[:] = old_qvals[:]
+    y[action] = update
+    loss = mse_loss_py(old_qvals, y) # Seems a bit funny... the loss is always the reward size...
+    total_loss += loss
+
+    # Store reward and transition state.
     action = np.argmax(qvals_eval)
     actions.append(action)
-
-    reward = get_reward(action, t, data)
+    
+    old_qvals = qvals_eval
+    old_state = state
+    
+    lstate = new_lstm_state_eval
+    state = data[t]
+    state[-2] = old_state[-2] + ACTION_VAL[action]
+    state[-1] = old_state[-1] + ACTION_VAL[action]*old_state[PRICE_INDEX]
+    agent_state = AgentState(new_state[-2], new_state[-1])
+    
+    reward, past_reward = get_reward(action, t, data, agent_state, past_reward)
     reward_sum += reward
     #rewards.append(reward)
 
-    lstate = new_lstm_state_eval
-    state = data[t:t+1]
-    
+
   print np.bincount(actions)
-  return reward_sum
+  return reward_sum, total_loss
 
 
 #
-# LSTM Definition
+# LSTM Definition with Fully Conncted output layer.
 #
-
-BATCH_SIZE = 1
-
-STEPSIZE = 1 
-
-#LSTM_HIDDEN_SIZE = 64
-LSTM_HIDDEN_SIZE = NUM_FEATURES
-
-#
-# Tensorflow LSTM with Fully Conncted output layer.
-#
-
 # Placeholders that are supplied each call:
 #   input for current step
 #   lstm hidden state from previous step
@@ -102,14 +224,14 @@ inputs = tf.placeholder(tf.float32, shape=(BATCH_SIZE,NUM_FEATURES), name="tf_in
 gold = tf.placeholder(tf.float32, shape=(BATCH_SIZE,NUM_ACTIONS), name="tf_gold")
 
 # c - LSTM speak
-lstm_state = tf.placeholder(tf.float32, shape=(BATCH_SIZE,2*LSTM_HIDDEN_SIZE), name="tf_lstm_state")
+lstm_state = tf.placeholder(tf.float32, shape=(BATCH_SIZE,FULL_LSTM_STATE_SIZE), name="tf_lstm_state")
 
 # Fully Connected layer weights and biases.
-FW = tf.Variable(tf.truncated_normal([NUM_ACTIONS, LSTM_HIDDEN_SIZE], stddev=0.2, mean=0, dtype=tf.float32), name="FW", dtype=tf.float32)
+FW = tf.Variable(tf.truncated_normal([NUM_ACTIONS, LSTM_STATE_SIZE], stddev=0.2, mean=0, dtype=tf.float32), name="FW", dtype=tf.float32)
 FB = tf.Variable(tf.truncated_normal([NUM_ACTIONS, 1], stddev=0.2, mean=0, dtype=tf.float32), name="FB", dtype=tf.float32)
 
 # LSTM model (weights are stored implicitly)
-lstm = tf.contrib.rnn.BasicLSTMCell(LSTM_HIDDEN_SIZE, state_is_tuple=False)
+lstm = tf.contrib.rnn.BasicLSTMCell(LSTM_STATE_SIZE, state_is_tuple=False)
 # TODO: add dropout 0.5
 
 # Prediction.
@@ -128,33 +250,28 @@ sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
 
+
+
+
 #
 # Q-Learning (Main).
 # 
 
 
-# Q-Learning Parameters
-gamma = 0.95  # discount factor
-alpha = 1     # learning rate
-epsilon = 1   # exploration factor
-
-# Global Parameters
-EPOCHS = 1500
-
-BUFFER_SIZE = 200
-
 replay_buffer = []
-action_history = []
-
-WINDOW_SIZE = 15
-
 
 # Train.
 for k in xrange(EPOCHS):
+  agent_state = INITIAL_AGENT_STATE
+
+  initial_value = agent_state.cash + (agent_state.stocks * data[0][PRICE_INDEX])
+  past_reward = RewardHistory(initial_value, initial_value, initial_value)
 
   # Initialize state and data.
-  lstate = np.random.normal(0, 0.2, 2*LSTM_HIDDEN_SIZE).astype(np.float32).reshape((1,2*NUM_FEATURES))
+  lstate = np.random.normal(0, 0.2, FULL_LSTM_STATE_SIZE).astype(np.float32).reshape((1,FULL_LSTM_STATE_SIZE))
   state = data[0]
+  state[-2] = agent_state.cash
+  state[-1] = agent_state.stocks
 
   # Run through data and update in batches of BUFFER_SIZE.
   for t in xrange(1, len(data)):
@@ -170,16 +287,18 @@ for k in xrange(EPOCHS):
     else: 
       action = np.argmax(qvals_eval)
     
+    # Take action, increment state.
+    new_state = data[t]
+    new_state[-2] = state[-2] + ACTION_VAL[action]
+    new_state[-1] = state[-1] + ACTION_VAL[action]*state[PRICE_INDEX]
+    agent_state = AgentState(new_state[-2], new_state[-1])
+
     # Observe reward. 
-    reward = get_reward(action, t, data)
+    reward, past_reward = get_reward(action, t, data, agent_state, past_reward)
     current_lstm_state = new_lstm_state_eval
 
-    # Increment state.
-    new_state = data[t:t+1]
-    action_history.append(action)
-
     # Store action.
-    cur_replay_step = (state, action, reward, new_state, qvals_eval, lstm_state, current_lstm_state)
+    cur_replay_step = (state, action, reward, new_state, qvals_eval, lstate, current_lstm_state)
     if (len(replay_buffer) < BUFFER_SIZE):
       replay_buffer.append(cur_replay_step)
     else:
@@ -191,10 +310,10 @@ for k in xrange(EPOCHS):
         #sess.run(qvals, feed_dict={inputs=old_state, lstm_state=old_lstate})
         #old_qvals = qvals
         qvals_eval = sess.run(qvals, feed_dict={inputs:new_state.reshape((1,NUM_FEATURES)), lstm_state:new_lstate})
-        new_qvals = qvals_eval
-        max_qval = np.max(new_qvals)
+        max_qval = np.max(qvals_eval)
         # Q-update
-        update = old_qvals[action] + alpha * (reward + gamma * max_qval - old_qvals[action])
+        #update = old_qvals[action] + alpha * (reward + gamma * max_qval - old_qvals[action])
+        update = alpha * (reward + (gamma * max_qval))
         y = np.zeros((NUM_ACTIONS, 1))
         y[:] = old_qvals[:]
         y[action] = update
@@ -207,7 +326,10 @@ for k in xrange(EPOCHS):
         x = X_train[j]
         y = y_train[j]
         cur_lstate = replay_buffer[j][-1]
-        sess.run(train_step, feed_dict={inputs:x.reshape((1,NUM_FEATURES)), gold:y.reshape((1,3)), lstm_state:cur_lstate.reshape((1,2*NUM_FEATURES))})
+        sess.run(train_step, \
+            feed_dict={inputs:x.reshape((1,NUM_FEATURES)), \
+                       gold:y.reshape((1,3)), \
+                       lstm_state:cur_lstate.reshape((1,FULL_LSTM_STATE_SIZE))})
 
       # Reset.
       replay_buffer = []
@@ -221,10 +343,13 @@ for k in xrange(EPOCHS):
   # Update exploration factor.
   if epsilon > 0.1:
     epsilon -= (1.0 / EPOCHS)
+  # Update future weight.
+  if gamma < 0.95 and gamma > 0.0:
+    gamma += (1.0 / EPOCHS)
 
   
-  reward_sum = max_reward(sess, data) 
-  print "Epoch #: {}\tReward {}".format(k, reward_sum) 
+  reward_sum, loss_sum = max_reward(sess, data) 
+  print "Epoch #: {}\tReward {}\tLoss {}".format(k, reward_sum, loss_sum) 
 
 # Test.
 
