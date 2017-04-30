4ef8334c24351d8e5cca76316ba8450e8bdb8f4c
Gene Kim <gkim21@cs.rochester.edu>
2017-04-29 21:54:04 -0400

    Added a bunch of dummy data files.

--- dummydata.py
+++ dummydata.py
@@ -7,13 +7,15 @@ import pandas as pd
 import random
 import matplotlib.pyplot as plt
 random.seed(123)
-data=[6100,6230]
-mu,sigma=0,100
+#data=[6100,6230]
+data=[20100,20230]
+mu,sigma=-1,1
 for i in range(3000):
-    data.append(data[-1]+50)
+    data.append(data[-1]-5)
+    #+np.random.normal(mu,sigma,1)[0])
     #if data[-1]>=6300 or data[-1]<=5900:
     #    data.append(6100)
     #else:data.append(np.random.normal(mu,sigma,1)[0]+data[-1])
     
-np.savetxt("data/linear_dummy.csv", data, delimiter=",")
+np.savetxt("data/linear_dummy_noise_-5_0.csv", data, delimiter=",")
 
