b7b60f18de518e3218469ca6360fb166a1eb5368
AlphaSue <409083575@qq.com>
2017-04-29 22:39:43 -0400

    Add files via upload

--- dummydata.py
+++ dummydata.py
@@ -1,21 +1,32 @@
 
-# coding: utf-8
-
-
 import numpy as np
 import pandas as pd
 import random
 import matplotlib.pyplot as plt
 random.seed(123)
-#data=[6100,6230]
-data=[20100,20230]
-mu,sigma=-1,1
-for i in range(3000):
-    data.append(data[-1]-5)
-    #+np.random.normal(mu,sigma,1)[0])
-    #if data[-1]>=6300 or data[-1]<=5900:
-    #    data.append(6100)
-    #else:data.append(np.random.normal(mu,sigma,1)[0]+data[-1])
-    
-np.savetxt("data/linear_dummy_noise_-5_0.csv", data, delimiter=",")
 
+#sma  simple moving average
+def calcSma(data, smaPeriod):
+    j = next(i for i, x in enumerate(data) if x is not None)
+    our_range = range(len(data))[j + smaPeriod - 1:]
+    empty_list = [None] * (j + smaPeriod - 1)
+    sub_result = [np.mean(data[i - smaPeriod + 1: i + 1]) for i in our_range]
+
+    return np.array(empty_list + sub_result)
+
+
+mu,sigma=10,2
+data=[6100,6230]
+for i in range(50):
+    data.append(data[-1]+np.random.normal(mu,sigma,1)[0])
+    
+data=np.array(data)    
+dif=np.diff(data)
+#dif means the difference of price between today and yesterday
+dif=np.insert(dif,0,0)
+#sma is the simple moving average of 15 days
+sma15=calcSma(data,15)
+d={'price':data,'dif':dif,'sma15':sma15}
+d=pd.DataFrame(d)
+d.fillna(0)
+d.to_csv('data/data_10__dif.csv',index=False)
